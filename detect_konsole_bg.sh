88ade68614f298529d3d8197d7c88363f22aa465
Razvan-Adrian Ciochina <rciochina@riverbed.com>
2023-10-25 21:34:43 +0300

    Add nvim config
    - since nvim is easier to deploy as appimage and has a lot of cool
      features I decided to heve it as my main editor for a while especially
      while learning RUST

--- detect_konsole_bg.sh
+++ detect_konsole_bg.sh
@@ -3,7 +3,7 @@ COLORSCHEME_WHITE="f9f9"
 old_stty_conf="$(stty -g)"
 stty -icanon -echo min 0 time 1 ; printf '\e]11;?\e\\'; read bg
 bg_term_val=$(echo $bg | cat -A)
-stty ${old_stty_conf}
+#stty ${old_stty_conf}
 if [ -n "${bg_term_val}" ]; then
 	echo "${bg_term_val##*rgb:}" | cut -d'/' -f1 | grep -q $COLORSCHEME_WHITE && echo "light"
 	exit 0
